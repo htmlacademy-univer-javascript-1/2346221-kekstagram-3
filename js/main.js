265d9134280c195956a8e3302f8e05b51468f681
Gloff228 <romo4ka101@gmail.com>
2023-03-11 10:55:41 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function getRandomInt(min, max) { //https://developer.mozilla.org/en-US/docs/Web
     min = Math.ceil(min);
     max = Math.floor(max);
     if (min === max) {
-        return min
+        return min;
     }
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
