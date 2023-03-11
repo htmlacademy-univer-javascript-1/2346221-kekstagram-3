64266ac8966790a0f1ca617b63d008b4e34f86a2
Gloff228 <romo4ka101@gmail.com>
2023-03-11 20:44:01 +0300

    Module 4 task 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/*
+Module 2 Task 1
+*/
+
 function getRandomInt(min, max) { //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   if (min > max) {
     const SWAP = max;
@@ -20,3 +24,28 @@ function checkLength(string, maxLength) {
 
 getRandomInt(1, 5);
 checkLength('123', 5);
+
+/*
+Module 4 Task 1
+*/
+
+const MAX_COUNT_OF_USERS = 25;
+const DESCRIPTION_LIST = ['Милые котята', 'Собачки', 'Новости', 'Веселое видео', 'Новая игра', 'Мем'];
+let numberOfLastPost = 0;
+
+function getRandomDescription() {
+  return DESCRIPTION_LIST[getRandomInt(0, DESCRIPTION_LIST.length - 1)]
+}
+
+function createPost() {
+  numberOfLastPost++;
+  return {
+    id: numberOfLastPost,
+    url: `photos/${numberOfLastPost}.jpg`,
+    description: getRandomDescription(),
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200)
+  }
+}
+
+const posts = Array.from({length: MAX_COUNT_OF_USERS}, createPost);
